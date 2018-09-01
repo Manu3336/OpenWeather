7d78d9adf89a8a33c671cee74b263899ca8c49c9
Manu Kapoor (Digital) <MA20018796@wipro.com>
2018-09-01 15:42:43 +0530

    commit webpack

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const  autoprefixer = require('autoprefixer');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: './src/indejx.js',
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        chunkFilename:'[id].js',
+        publicPath: ''
+    }, 
+    resolve:{
+        extensions: ['.js', '.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                loader:'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test:/\.css$/,
+                exclude: /node_modules/,
+                use:[
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options:{
+                            importLoaders:1,
+                            modules: true,
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    },
+                    {
+                        loader:'postcss-loader',
+                        options:{
+                            ident: 'postcss',
+                            plugin:() =>[
+                                autoprefixer({
+                                    "browsers":[
+                                        "> 1%",
+                                        "last 2 versions"
+                                    ]
+                                })
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebPackPlugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
